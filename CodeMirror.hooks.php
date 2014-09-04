b70b84a76234a6a808bfa232e46a1908b004aef1
Pavel Astakhov <pastakhov@yandex.ru>
2014-09-04 09:37:11 +0600

    fix variables that was added with hook MagicWordwgVariableIDs (v 1.11.2)
    Change-Id: Ic40e1e055e71585ed3e1706f356eafe7817f35a2

--- CodeMirror.hooks.php
+++ CodeMirror.hooks.php
@@ -8,17 +8,30 @@ class CodeMirrorHooks {
 	/**
 	 *
 	 * @global Parser $wgParser
+	 * @global Language $wgContLang
 	 * @param EditPage $editPage
 	 * @param OutputPage $output
 	 * @return boolean
 	 */
 	public static function onEditPageShowEditFormInitial( EditPage $editPage, OutputPage $output ) {
-		global $wgParser;
+		global $wgParser, $wgContLang;
 		if ( false === isset( $wgParser->mFunctionSynonyms ) ) {
+			$wgParser->initialiseVariables();
 			$wgParser->firstCallInit();
 		}
-		self::$globalVariableScript['FunctionSynonyms'] = $wgParser->mFunctionSynonyms;
 		self::$globalVariableScript['Tags'] = $wgParser->getTags();
+
+		$mw = $wgContLang->getMagicWords();
+		self::$globalVariableScript['FunctionSynonyms'] = $wgParser->mFunctionSynonyms;
+		foreach ( MagicWord::getVariableIDs() as $name ) {
+			if ( isset( $mw[$name] ) ) {
+				$caseSensitive = array_shift( $mw[$name] ) == 0 ? 0 : 1;
+				foreach ( $mw[$name] as $n ) {
+					self::$globalVariableScript['FunctionSynonyms'][$caseSensitive][ $caseSensitive ? $n : $wgContLang->lc( $n ) ] = $name;
+				}
+			}
+		}
+
 		$output->addModules( 'ext.CodeMirror' );
 		return true;
 	}
