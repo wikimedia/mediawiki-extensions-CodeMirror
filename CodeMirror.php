9414e19bea366ae96aca0367ecc60343960bf3f5
Pavel Astakhov <pastakhov@yandex.ru>
2015-10-29 08:57:01 +0600

    Skip setCodeEditorPreference for anon users (v 3.2.2.24)
    Change-Id: I4064a2ae849ab5efc2cab3bbb2c503568d490b3b

--- CodeMirror.php
+++ CodeMirror.php
@@ -15,7 +15,7 @@ if ( !defined( 'MEDIAWIKI' ) ) {
 	die( 'This file is an extension to MediaWiki and thus not a valid entry point.' );
 }
 
-const EXT_CODEMIRROR_VERSION = '3.2.1';
+const EXT_CODEMIRROR_VERSION = '3.2.2.24';
 
 // Register this extension on Special:Version
 $wgExtensionCredits['parserhook'][] = array(
