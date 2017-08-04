a7499458976a5c1bbdeed546e82fce79f70cf0aa
Max Semenik <maxsem.wiki@gmail.com>
2017-08-03 17:54:28 -0700

    ext.CodeMirror.loader is unused, remove
    Change-Id: I2b51fb2e4e20fef088503567ba0cf818a1d2c857

--- resources/ext.CodeMirror.loader.js
+++
@@ -1,7 +0,0 @@
-( function ( mw ) {
-	var config = mw.config.get( 'extCodeMirrorConfig' );
-
-	if ( config.pluginModules && config.pluginModules.length > 0 ) {
-		mw.loader.load( config.pluginModules );
-	}
-}( mediaWiki ) );
