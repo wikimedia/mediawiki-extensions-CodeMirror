63647bb5dc4f637bb7facaec22fcc10ca58ec097
Max Semenik <maxsem.wiki@gmail.com>
2018-06-13 15:22:37 -0700

    Remove all BetaFeatures support
    Bug: T185030
    Change-Id: I1ad451acfd163c783e7b8ec604d8695c14342667

--- includes/CodeMirrorHooks.php
+++ includes/CodeMirrorHooks.php
@@ -3,32 +3,6 @@
 class CodeMirrorHooks {
 
 	/**
-	 * Checks if CodeMirror is enabled for this user
-	 *
-	 * @param IContextSource $context The current ContextSource object
-	 * @staticvar null|boolean $isEnabled Saves, if CodeMirror should be loaded on this page or not
-	 * @return bool
-	 */
-	private static function isCodeMirrorEnabled( IContextSource $context ) {
-		global $wgCodeMirrorBetaFeature;
-		static $isEnabled = null;
-
-		// Check, if we already checked, if page action is editing, if not, do it now
-		if ( $isEnabled === null ) {
-			if ( !$wgCodeMirrorBetaFeature ) {
-				$isEnabled = true;
-			} else {
-				$isEnabled = $wgCodeMirrorBetaFeature &&
-					ExtensionRegistry::getInstance()->isLoaded( 'BetaFeatures' ) &&
-					BetaFeatures::isFeatureEnabled(
-						$context->getUser(), 'codemirror-syntax-highlight' );
-			}
-		}
-
-		return $isEnabled;
-	}
-
-	/**
 	 * Checks if CodeMirror for textarea wikitext editor should be loaded on this page or not.
 	 *
 	 * @param IContextSource $context The current ContextSource object
@@ -38,8 +12,7 @@ class CodeMirrorHooks {
 	private static function isCodeMirrorOnPage( IContextSource $context ) {
 		return in_array( Action::getActionName( $context ), [ 'edit', 'submit' ] ) &&
 			// CodeMirror on textarea wikitext editors doesn't support RTL (T170001)
-			!$context->getTitle()->getPageLanguage()->isRTL() &&
-			self::isCodeMirrorEnabled( $context );
+			!$context->getTitle()->getPageLanguage()->isRTL();
 	}
 
 	/**
@@ -54,7 +27,8 @@ class CodeMirrorHooks {
 		if ( self::isCodeMirrorOnPage( $out->getContext() ) ) {
 			$out->addModules( 'ext.CodeMirror' );
 		}
-		$out->addJsConfigVars( 'wgCodeMirrorEnabled', self::isCodeMirrorEnabled( $out->getContext() ) );
+		// TODO: for backwards compatibility. Remove in 2019.
+		$out->addJsConfigVars( 'wgCodeMirrorEnabled', true );
 	}
 
 	/**
@@ -72,33 +46,6 @@ class CodeMirrorHooks {
 			'type' => 'api',
 			'default' => '1',
 		];
-	}
-
-	/**
-	 * GetBetaFeaturePreferences hook handler
-	 *
-	 * @param User $user
-	 * @param array &$preferences
-	 */
-	public static function onGetBetaFeaturePreferences( User $user, &$preferences ) {
-		global $wgCodeMirrorBetaFeature, $wgExtensionAssetsPath;
-		if ( $wgCodeMirrorBetaFeature ) {
-			$preferences['codemirror-syntax-highlight'] = [
-				'label-message' => 'codemirror-beta-title',
-				'desc-message' => 'codemirror-beta-desc',
-				'screenshot' => [
-					'ltr' => $wgExtensionAssetsPath . '/CodeMirror/resources/images/codemirror-beta-ltr.svg',
-					'rtl' => $wgExtensionAssetsPath . '/CodeMirror/resources/images/codemirror-beta-rtl.svg'
-				],
-				'info-link' => 'https://meta.wikimedia.org/wiki/Special:MyLanguage/' .
-					'Community_Tech/Wikitext_editor_syntax_highlighting',
-				'discussion-link' => 'https://www.mediawiki.org/wiki/' .
-					'Extension_talk:CodeMirror',
-				'requirements' => [
-					'javascript' => true,
-				],
-			];
-		}
 	}
 
 	/**
