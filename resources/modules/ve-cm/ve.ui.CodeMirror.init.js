89f52f6fbbcd9c6d3b1ab8905a0749b07807316c
Ed Sanders <esanders@wikimedia.org>
2020-06-30 16:21:33 +0100

    VE: Add explicit dependency on targetLoader
    Change-Id: I3d67837b462e0b79e124eff158bd7f88b5aaf63e

--- resources/modules/ve-cm/ve.ui.CodeMirror.init.js
+++ resources/modules/ve-cm/ve.ui.CodeMirror.init.js
@@ -1,4 +1,4 @@
-( function () {
+mw.loader.using( 'ext.visualEditor.targetLoader' ).then( function () {
 	mw.libs.ve.targetLoader.addPlugin( function () {
 		var index,
 			target = ve.init.mw.DesktopArticleTarget;
@@ -8,4 +8,4 @@
 			target.static.actionGroups[ 1 ].include.splice( index, 0, 'codeMirror' );
 		}
 	} );
-}() );
+} );
