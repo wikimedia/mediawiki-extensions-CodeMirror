47e42bc61b2fe0cfcfef8a6c2b4c71d23089e704
Ed Sanders <esanders@wikimedia.org>
2023-02-14 17:30:33 -0800

    Search for toolbar place in toolbarConfig as well as actionConfig
    The actionToolbar is deprecated and will be going away soon.
    
    Change-Id: I475e987959a037eaa536986cba063ab3ccc0083e

--- resources/modules/ve-cm/ve.ui.CodeMirror.init.js
+++ resources/modules/ve-cm/ve.ui.CodeMirror.init.js
@@ -1,10 +1,16 @@
 mw.loader.using( 'ext.visualEditor.targetLoader' ).then( function () {
 	mw.libs.ve.targetLoader.addPlugin( function () {
 		var target = ve.init.mw.DesktopArticleTarget;
-
 		if ( target ) {
-			var index = target.static.actionGroups[ 1 ].include.indexOf( 'changeDirectionality' );
-			target.static.actionGroups[ 1 ].include.splice( index, 0, 'codeMirror' );
+			var groups = target.static.toolbarGroups.concat( target.static.actionGroups );
+			groups.some( function ( group ) {
+				if ( group.name === 'pageMenu' ) {
+					var index = group.include.indexOf( 'changeDirectionality' );
+					group.include.splice( index, 0, 'codeMirror' );
+					return true;
+				}
+				return false;
+			} );
 		}
 	} );
 } );
