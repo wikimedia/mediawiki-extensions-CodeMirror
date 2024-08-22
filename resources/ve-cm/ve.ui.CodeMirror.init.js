e439fb02eb59560e0c16476143efbe975e33b441
Bartosz Dziewoński <dziewonski@fastmail.fm>
2024-08-23 00:05:43 +0200

    Fix too many '..' in require()
    Bug: T373065
    Bug: T373153
    Change-Id: Ia00add5d98173e2c3d8568e92c427864014e7257

--- resources/ve-cm/ve.ui.CodeMirror.init.js
+++ resources/ve-cm/ve.ui.CodeMirror.init.js
@@ -1,4 +1,4 @@
-require( '../../ext.CodeMirror.data.js' );
+require( '../ext.CodeMirror.data.js' );
 
 if ( mw.config.get( 'extCodeMirrorConfig' ).useV6 ) {
 	mw.loader.load( 'ext.CodeMirror.v6.visualEditor' );
