5ff54cf7f6b0c72181b1c1d6c556565842268aee
MusikAnimal <musikanimal@gmail.com>
2024-09-04 18:27:21 -0400

    ve.ui.CodeMirror.init: make cm6enable=1 query param work for 2017 editor
    Change-Id: I13f31aca84ffd953de31d5ed18c658e9620f72a1

--- resources/ve-cm/ve.ui.CodeMirror.init.js
+++ resources/ve-cm/ve.ui.CodeMirror.init.js
@@ -1,6 +1,8 @@
 require( '../ext.CodeMirror.data.js' );
 
-if ( mw.config.get( 'extCodeMirrorConfig' ).useV6 ) {
+const urlParams = new URLSearchParams( window.location.search );
+
+if ( mw.config.get( 'extCodeMirrorConfig' ).useV6 || urlParams.get( 'cm6enable' ) ) {
 	mw.loader.load( 'ext.CodeMirror.v6.visualEditor' );
 } else {
 	mw.loader.load( 'ext.CodeMirror.visualEditor' );
